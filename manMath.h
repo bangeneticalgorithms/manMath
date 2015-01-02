0c3bc2d6a7d60101a2d7ff228810b1dd78ddc7b1
Isuru Daulagala <iud23drexel@gmail.com>
2015-01-02 10:33:00 -0800

    Added few more prototypes for manMath

--- manMath.h
+++ manMath.h
@@ -2,11 +2,29 @@
 #define __MANMATH_H__
 
 #include "point.h"
+#include "vector.h"
+#include "segment.h"
+
+#include "manMath_utils.h"
 
 MANMATH_NAMESPACE_BEGIN
 
+/*Manhattan distance between 2 points */
 template <typename T>
-T getDistance(Point<T>* p1, Point<T>* p2);
+T getDistanceP2P(Point<T>* p1, Point<T>* p2)
+{
+    return ABS(p1->X - p2->X) + ABS(p1->Y - p2->Y);
+}
+
+/*Get the list of points/Manhattan arc that 
+ * are equidistant from 2 Points */
+template <typename T>
+segment<T>* p2pManArc(Point<T>* p1, Point<T>* p2);
+
+/* get the manhattan distance between two line segments*/
+template <typename T>
+T getDistanceL2L(Point<T>* p1, Point<T>* p2);
+
 
 MANMATH_NAMESPACE_END
 
