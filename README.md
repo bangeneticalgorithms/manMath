514a483897b57845ec2e3381157c7dcff1e7068f
Isuru Daulagala <iud23drexel@gmail.com>
2015-01-21 12:32:36 -0800

    Initial commit, added README and struct

---
+++ README.md
@@ -0,0 +1,7 @@
+
+manMath is a library for Manhattan/Taxicab/Rectiliear geometry functions
+
+
+
+Header Files:
+    point.h - The data structure for point is defined here
---
+++ point.h
@@ -0,0 +1,16 @@
+/* This header file contains the struct for a point
+ * the type of X and Y could be of any numerical type
+ * as long as they are of the same type
+ */
+
+#ifndef __POINT_H__
+#define __POINT_H__
+
+template <typename T>
+struct Point
+{
+    T X;
+    T T;
+}
+
+#endif
