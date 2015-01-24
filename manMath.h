74ecb71774bd3193da325b2551bd08ca77e7452c
Isuru Daulagala <iud23drexel@gmail.com>
2015-01-24 14:56:26 -0800

    Added a default header, constructor

---
+++ manMath.h
@@ -0,0 +1,15 @@
+#ifndef __MANMATH_H__
+#define __MANMATH_H__
+
+#include "point.h"
+
+namespace manMath_lib
+{
+    
+    template <typename T>
+    T getDistance(Point<T>* p1, Point<T>* p2);
+
+}
+
+#endif
+
