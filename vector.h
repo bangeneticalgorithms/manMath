c7fe9d3377c0b63f974bdea2590bcdc810c77ab5
Isuru Daulagala <iud23drexel@gmail.com>
2015-01-27 10:32:00 -0800

    Added struct for vector

---
+++ vector.h
@@ -0,0 +1,15 @@
+#ifndef __VECTOR_H__
+#define __VECTOR_H__
+
+template <typename T>
+struct Vector
+{
+    T R;
+    T S;
+
+    /*Constructor */
+    Vector(T r, T s): R(r), S(s) {};
+
+}
+
+#endif
