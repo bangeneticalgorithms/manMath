64c2cd86e23d033f20eb1b96c1e910dec02dc5ef
Isuru Daulagala <iud23drexel@gmail.com>
2015-01-31 12:13:00 -0800

    Added another constructor for segment, converteed t to double

--- segment.h
+++ segment.h
@@ -13,17 +13,30 @@ class Segment
 
         /*Constructor with two points
          * This constructor finds the parametric form of a line segment
-         * when it is constructed with two points*/
-        Segment(Point<T>* p1, Point<T>* p2)
+         * when it is constructed with two points
+         * Take first point as the starting point
+         * and the second point is the ending point
+         * Hence => tMin =0 tMax =1*/
+        Segment(Point<T>* p1, Point<T>* p2):R(p1), tMin(0.0), tMax(1.0)
         {
+            /*Then use the two points to calculate V*/
+            T _x = p2->X - p1->X;
+            T _y = p2->Y - p1->Y;
 
+            V = new Vector(_x,_y);
         }
 
     private:
-        Vector* V; 
+        /* A line can be represented as
+         * R+t*V 
+         * t is parametrisized*/
+        Vector* V;  
         Point* R;
-        T tMax;
-        T tMin;
+
+        /*A line segment is essentially a line which has a bound on the
+         * parametric */
+        double tMax;
+        double tMin;
 
 
 };
