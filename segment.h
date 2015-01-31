6b1acf03fc3ee6fc406c75154ffb9c8e381c6c60
Isuru Daulagala <iud23drexel@gmail.com>
2015-01-31 15:36:00 -0800

    Converted segment to a class, Added a def constructor

--- segment.h
+++ segment.h
@@ -1,13 +1,29 @@
 #ifndef __SEGMENT_H__
 #define __SEGMENT_H__
 
-template <typename T>
-struct Segment
-{
-    Vector* V;
-    Point* R;
+#include "point.h"
+#include "vector.h"
 
-    T tMax;
-    T tMin;
+template <typename T>
+class Segment
+{
+    public:
+        /*Default Constructor*/
+        Segment(Vector<T>* v, Point<T>* r, T max, T min):V(v), R(r), tMax(max), tMin(min){}
+
+        /*Constructor with two points
+         * This constructor finds the parametric form of a line segment
+         * when it is constructed with two points*/
+        Segment(Point<T>* p1, Point<T>* p2)
+        {
+
+        }
+
+    private:
+        Vector* V; 
+        Point* R;
+        T tMax;
+        T tMin;
+
 
 };
