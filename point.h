bd2906a9881216ab2f356b3c3c5181dc69d545bc
Isuru Daulagala <iud23drexel@gmail.com>
2015-02-04 14:41:31 -0800

    Added a function in point.h to handle if inbetweenness

--- point.h
+++ point.h
@@ -12,7 +12,21 @@ struct Point
     T X;
     T Y;
 
-    point(T x, T y):X(x), Y(y){}
+    Point(T x, T y):X(x), Y(y){}
 };
 
+/*Is a given point gP in between 
+ * two target points tP1 and tP2
+ */
+template <typename T>
+bool inBetween(Point<T>* gP, Point<T>* tP1, Point<T>* tP2)
+{
+    T maxX = (tP1->X > tP2->X) ? tP1->X : tP2->X ;
+    T maxY = (tP1->Y > tP2->Y) ? tP1->Y : tP2->Y ;
+    T minX = (tP2->X > tP1->X) ? tP1->X : tP2->X ;
+    T minY = (tP2->Y > tP1->Y) ? tP1->Y : tP2->Y ;
+
+    return ((gP->X > minX) &&(gP->X < maxX)) || ((gP->Y > minY) &&(gP->X < maxY));
+
+}
 #endif
